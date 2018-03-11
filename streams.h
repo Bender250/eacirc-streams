d05afd7c096b5d8bb6eae9ef168b1dc7b58e9068
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-11 10:50:39 +0100

    sac-step fix

--- streams.h
+++ streams.h
@@ -172,8 +172,8 @@ struct sac_step_stream : sac_stream {
 
         } else {
             _step = 0;
-            std::uniform_int_distribution<std::size_t> dist{0, osize() / 2 * 8};
-            std::size_t pos = dist(_rng) + osize() / 2 * 8;
+            std::uniform_int_distribution<std::size_t> dist{0, osize() * 8};
+            std::size_t pos = dist(_rng) + osize() * 8;
             _data[pos / 8] ^= (1 << (pos % 8));
         }
 
