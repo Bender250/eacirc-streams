e13eea38db63e1fdd408efa6c718c7d6ce33398a
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-22 20:37:05 +0100

    sha1 base

---
+++ streams/hashes/functions/SHA1/sha1.h
@@ -0,0 +1,50 @@
+//
+// Created by Dusan Klinec on 22/03/2018.
+// https://raw.githubusercontent.com/clibs/sha1/master/sha1.h
+//
+
+#ifndef EACIRC_STREAMS_SHA1_H
+#define EACIRC_STREAMS_SHA1_H
+
+/*
+   SHA-1 in C
+   By Steve Reid <steve@edmweb.com>
+   100% Public Domain
+ */
+
+#include <cstdint>
+
+typedef struct
+{
+    uint32_t state[5];
+    uint32_t count[2];
+    unsigned char buffer[64];
+} SHA1_CTX;
+
+void SHA1Transform(
+    uint32_t state[5],
+    const unsigned char buffer[64]
+);
+
+void SHA1Init(
+    SHA1_CTX * context
+);
+
+void SHA1Update(
+    SHA1_CTX * context,
+    const unsigned char *data,
+    uint32_t len
+);
+
+void SHA1Final(
+    unsigned char digest[20],
+    SHA1_CTX * context
+);
+
+void SHA1(
+    char *hash_out,
+    const char *str,
+    int len);
+
+
+#endif //EACIRC_STREAMS_SHA1_H
