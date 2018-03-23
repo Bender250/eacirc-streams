9e46e76a4f81281f89a8b2c6c9abd200f38b3356
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-23 14:26:02 +0100

    ripemd160 round reduced

--- streams/hashes/functions/Ripemd160/ripemd160.h
+++ streams/hashes/functions/Ripemd160/ripemd160.h
@@ -1,6 +1,9 @@
 //
 // Created by Dusan Klinec on 23/03/2018.
 // https://homes.esat.kuleuven.be/~bosselae/ripemd160.html
+// f("") = 9c1185a5c5e9fc54612808977ee8f548b2258d31
+// f("a") = 0bdc9d2d256b3ee9daae347be6f4dc835a467ffe
+// f("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") = 9b752e45573d4b39f4dbd3323cab82bf63326bfb
 
 /* ripemd-160.h */
 #ifndef  RMD160_H
@@ -26,8 +29,8 @@ typedef struct ripemd160_ctx
 /* hash functions */
 
 void rhash_ripemd160_init(ripemd160_ctx *ctx);
-void rhash_ripemd160_update(ripemd160_ctx *ctx, const unsigned char* msg, size_t size);
-void rhash_ripemd160_final(ripemd160_ctx *ctx, unsigned char result[20]);
+void rhash_ripemd160_update(ripemd160_ctx *ctx, const unsigned char* msg, size_t size, unsigned nr);
+void rhash_ripemd160_final(ripemd160_ctx *ctx, unsigned char result[20], unsigned nr);
 
 #ifdef __cplusplus
 } /* extern "C" */
