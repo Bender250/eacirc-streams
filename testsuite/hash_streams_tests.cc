5868f83f1405fd4fa2b2ea5ec6fe3523d8752b42
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-23 17:56:06 +0100

    hash test cases

---
+++ testsuite/hash_streams_tests.cc
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <eacirc-core/json.h>
+#include <streams/hashes/hash_factory.h>
+#include <streams/hashes/hash_interface.h>
+#include <fstream>
+#include "stream.h"
+#include "streams.h"
+
+#include "testsuite/test-utils/hash_test_case.h"
+
+/** Source of test vectors http://csrc.nist.gov/groups/ST/hash/sha-3/index.html */
+
+//
+//TEST(gost, test_vectors) {
+//    testsuite::hash_test_case("Gost", 16)();
+//}
+
+TEST(sha1, test_vectors) {
+    testsuite::hash_test_case("SHA1", 80)();
+}
+
+TEST(sha256, test_vectors) {
+    testsuite::hash_test_case("SHA256", 64)();
+}
+
+TEST(md5, test_vectors) {
+    testsuite::hash_test_case("MD5", 64)();
+}
